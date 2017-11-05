f6d6997ee7e549d2833655a0fcdc59298b9d8efd
fazilbinzaid <fazilibnzaid@gmail.com>
2017-11-05 12:18:41 +0530

    requirements

--- settings/heroku.py
+++ settings/heroku.py
@@ -8,8 +8,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config()
@@ -20,4 +18,31 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK['PAGE_SIZE'] = 15
 
+INSTALLED_APPS += [
+
+    'debug_toolbar',
+
+]
+
+MIDDLEWARE += [
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+]
+
 INTERNAL_IPS = [ '127.0.0.1' ]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
